ff2add163d6f6b2158f6da9ad95fc000883e6d44
Easter1995 <1073208733@qq.com>
2024-05-28 18:45:28 +0800

    finish all

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ import jpcap.NetworkInterface;
 import jpcap.PacketReceiver;
 import jpcap.packet.*;
 
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.event.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -12,12 +17,20 @@ import java.util.concurrent.Executors;
  * 主类
  */
 public class Main {
+    /* 抓包选项 */
+    public static String selectedOption;
+    /* 现在是否正在抓包 */
+    public static volatile boolean isCapturing = true;
     public static void main(String[] args) {
+        Main mainObj = new Main();
         /* 开窗口 */
-        new NPCWindow();
+        NPCWindow window = new NPCWindow();
         try {
             /* 获取网络接口列表 */
             NetworkInterface[] devices = JpcapCaptor.getDeviceList();
+
+            /* 创建线程池，避免为同一个接口创建多个线程 */
+            ExecutorService threadPool = Executors.newFixedThreadPool(devices.length);
 
             /* 没找到本机的网络设备 */
             if (devices.length == 0) {
@@ -25,34 +38,112 @@ public class Main {
                 return;
             }
 
-            /* 创建线程池，对每个网络接口进行抓包 */
-            ExecutorService threadPool = Executors.newFixedThreadPool(devices.length);
+            /* 将网络接口名称添加到 JComboBox 中 */
             for (NetworkInterface device : devices) {
-                Runnable task = () -> NetworkPacketCap(device);
-                threadPool.execute(task);
+                window.chooseDevice.addItem(device.name + " - " + device.description);
             }
-            /* 关闭线程池 */
-            threadPool.shutdown();
-            threadPool.shutdownNow();
+
+            /* 设置选项监听器, 获取选项框被选中的选项 */
+            window.chooseDevice.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    isCapturing = false;
+                    NPCWindow.statusArea.setText("加载接口，当前抓包暂停中...");
+                    JComboBox<String> comboBox = (JComboBox<String>) e.getSource();
+                    selectedOption = (String) comboBox.getSelectedItem();
+                }
+            });
+            /* 根据被选中的选项来对特定接口进行抓包 */
+            window.startBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    for (NetworkInterface device : devices) {
+                        if (selectedOption.equals(device.name + " - " + device.description) || selectedOption.equals("all")) {
+                            // 改变这个值使得当前线程可以结束
+                            isCapturing = false;
+                            if (!selectedOption.equals("all")) {
+                                // 等待一段时间，以确保之前的抓包线程能够结束
+                                try {
+                                    Thread.sleep(1000); // 1秒钟
+                                } catch (InterruptedException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+
+                            // 提交任务给线程池执行
+                            threadPool.submit(() -> mainObj.NetworkPacketCap(device));
+                        }
+                    }
+                }
+            });
+            /* 清空屏幕 */
+            window.clearBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    mainObj.clearTable();
+                }
+            });
+            /* 根据选中的表格条目，来显示特定的包的payload */
+            NPCWindow.table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+                @Override
+                public void valueChanged(ListSelectionEvent e) {
+                    if (!e.getValueIsAdjusting()) {
+                        int selectedRow = NPCWindow.table.getSelectedRow();
+                        if (selectedRow != -1) {
+                            // 获取选中行的某一列的数据
+                            String dataByte = (String) NPCWindow.model.getValueAt(selectedRow, 6);
+                            String dataStr = (String) NPCWindow.model.getValueAt(selectedRow, 7);
+
+                            NPCWindow.byteArea.setText(dataByte);
+                            NPCWindow.txtArea.setText(dataStr);
+                        }
+                    }
+                }
+            });
+
+            // 在程序退出时执行的关闭逻辑
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                if (threadPool != null && !threadPool.isShutdown()) {
+                    threadPool.shutdown(); // 关闭线程池
+                }
+            }));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+    /**
+     * 清空表项
+     */
+    private void clearTable() {
+        NPCWindow.model.setRowCount(0);
+        NPCWindow.byteArea.setText("");
+        NPCWindow.txtArea.setText("");
+    }
     /**
      * 对单独一个网络接口进行抓包
      * @param device 网络接口
      */
-    private static void NetworkPacketCap(NetworkInterface device) {
+    private void NetworkPacketCap(NetworkInterface device) {
         System.out.println("This is Thread: " + Thread.currentThread());
+        isCapturing = true;
+        clearTable();
         try {
             // 打开网卡连接, 捕获时长为1s，默认开启混杂模式
             JpcapCaptor jpcapCaptor = JpcapCaptor.openDevice(device, 65535, true, 1000);
-            jpcapCaptor.processPacket(5, new PacketHandler());
+            if (selectedOption.equals("all")) {
+                SwingUtilities.invokeLater(() -> NPCWindow.statusArea.setText("当前接口：所有接口，抓包中..."));
+            } else {
+                NPCWindow.statusArea.setText("当前接口：" + device.name + "-" + device.description + " 抓包中...");
+            }
+
+            while (isCapturing) {
+                jpcapCaptor.processPacket(1, new PacketHandler());
+            }
             // 关闭捕获器，释放资源
             jpcapCaptor.close();
         } catch (Exception e) {
             // 异常处理
+            isCapturing = false;
             System.out.println("catch exception in thread" + Thread.currentThread());
         }
     }
@@ -69,42 +160,127 @@ class PacketHandler implements PacketReceiver {
      * **/
     @Override
     public void receivePacket (Packet packet) {
+        String filterInput = NPCWindow.getFilterField().getText();
         // 分析IP包
         if (packet.getClass().equals(IPPacket.class)) {
             // IP数据包, IPPacket类继承 Packet类,包括 IPV4和 IPV6;
             IPPacket ipPacket = (IPPacket) packet;
-            // 数据部分
-            byte[] dataByte = ipPacket.data;
-            String hex = this.toHexString(dataByte);
-            String str = this.toCharString(dataByte);
 
-            System.out.println("ip data: " + hex);
-            System.out.println("data: " + str);
+            if (filterInput.equals("SRC" + ipPacket.src_ip.toString()) ||
+                filterInput.equals("DST" + ipPacket.dst_ip.toString()) ||
+                filterInput.equals("IP") ||
+                filterInput.isEmpty()) {
+                // 数据部分
+                byte[] dataByte = ipPacket.data;
+                String hex = this.toHexString(dataByte);
+                String str = this.toCharString(dataByte);
+                // 时间戳
+                double time = (double) ipPacket.sec + (double) ipPacket.usec / 1000;
+                String timeStr = String.format("%.2f", time);
+                // 源ip，目的ip
+                String srcAddress = ipPacket.src_ip.toString();
+                String dstAddress = ipPacket.dst_ip.toString();
+                // 协议
+                String pro = "IP";
+                // 长度
+                String len = String.valueOf(ipPacket.data.length);
+                // 信息
+                String info = "TTL: " + ipPacket.hop_limit + " " + "Identification: " + ipPacket.ident + "Version: " + " " + ipPacket.version;
+                // 加入table
+                addToTable(timeStr, srcAddress, dstAddress, pro, len, info, hex, str);
+
+                System.out.println("ip data: " + hex);
+                System.out.println("data: " + str);
+            }
         }
         // 分析TCP包
         if (packet.getClass().equals(TCPPacket.class)) {
-            // IP数据包, IPPacket类继承 Packet类,包括 IPV4和 IPV6;
             TCPPacket tcpPacket = (TCPPacket) packet;
-            // 数据部分
-            byte[] dataByte = tcpPacket.data;
-            String hex = this.toHexString(dataByte);
-            String str = this.toCharString(dataByte);
 
-            System.out.println("tcp data: " + hex);
-            System.out.println("data: " + str);
+            if (filterInput.equals("SRC" + tcpPacket.src_ip.toString()) ||
+                    filterInput.equals("DST" + tcpPacket.dst_ip.toString()) ||
+                    filterInput.equals("TCP") ||
+                    filterInput.isEmpty()) {
+                // 数据部分
+                byte[] dataByte = tcpPacket.data;
+                String hex = this.toHexString(dataByte);
+                String str = this.toCharString(dataByte);
+                // 时间戳
+                double time = (double) tcpPacket.sec + (double) tcpPacket.usec / 1000000.0;
+                String timeStr = String.format("%.2f", time);
+                // 源ip，目的ip
+                String srcAddress = tcpPacket.src_ip.toString();
+                String dstAddress = tcpPacket.dst_ip.toString();
+                // 协议
+                String pro = "TCP";
+                // 长度
+                String len = String.valueOf(tcpPacket.data.length);
+                // 信息
+                String info = "ACK= " + tcpPacket.ack_num + ", " + "SEQ= " + tcpPacket.sequence + ", " + "window size: " + tcpPacket.window;
+                // 加入table
+                addToTable(timeStr, srcAddress, dstAddress, pro, len, info, hex, str);
+
+                System.out.println("tcp data: " + hex);
+                System.out.println("data: " + str);
+            }
         }
         // 分析UDP包
         if (packet.getClass().equals(UDPPacket.class)) {
             // UDP数据包
             UDPPacket udpPacket = (UDPPacket) packet;
-            // 数据部分
-            byte[] dataByte = udpPacket.data;
-            String hex = this.toHexString(dataByte);
-            String str = this.toCharString(dataByte);
 
-            System.out.println("udp data: " + hex);
-            System.out.println("data: " + str);
+            if (filterInput.equals("SRC" + udpPacket.src_ip.toString()) ||
+                    filterInput.equals("DST" + udpPacket.dst_ip.toString()) ||
+                    filterInput.equals("UDP") ||
+                    filterInput.isEmpty()) {
+                // 数据部分
+                byte[] dataByte = udpPacket.data;
+                String hex = this.toHexString(dataByte);
+                String str = this.toCharString(dataByte);
+                // 时间戳
+                double time = (double) udpPacket.sec + (double) udpPacket.usec / 1000;
+                String timeStr = String.format("%.2f", time);
+                // 源ip，目的ip
+                String srcAddress = udpPacket.src_ip.toString();
+                String dstAddress = udpPacket.dst_ip.toString();
+                // 协议
+                String pro = "UDP";
+                // 长度
+                String len = String.valueOf(udpPacket.data.length);
+                // 信息
+                String info = "protocol: " + inferProtocol(udpPacket.src_port, udpPacket.dst_port, str) + ", " + "total length= " + udpPacket.caplen;
+                // 加入table
+                addToTable(timeStr, srcAddress, dstAddress, pro, len, info, hex, str);
+
+                System.out.println("udp data: " + hex);
+                System.out.println("data: " + str);
+            }
         }
+    }
+
+    /**
+     * 为表格添加一行元素
+     * @param time 时间戳
+     * @param srcip 源ip
+     * @param dstip 目的ip
+     * @param pro 协议
+     * @param len 长度
+     * @param info 信息
+     */
+    public void addToTable(String time, String srcip, String dstip, String pro, String len, String info, String dataByte, String dataStr) {
+        Vector<String> dataVec = new Vector<String>();
+        dataVec.add(time);
+        dataVec.add(srcip);
+        dataVec.add(dstip);
+        dataVec.add(pro);
+        dataVec.add(len);
+        dataVec.add(info);
+        dataVec.add(dataByte);
+        dataVec.add(dataStr);
+        // 将更新表格数据任务添加进事件调度线程的队列中等待执行
+        SwingUtilities.invokeLater(() -> {
+            NPCWindow.model.addRow(dataVec);
+        });
     }
     /**
      * 将字节数组转换为 utf-8 编码的字符串，只转换能转换的字符
@@ -139,5 +315,24 @@ class PacketHandler implements PacketReceiver {
             hexString.append(String.format("%02X ", b));
         }
         return hexString.toString();
+    }
+    /**
+     *
+     * @param sourcePort 源端口
+     * @param destinationPort 目的端口
+     * @param str 报文
+     * @return 协议
+     */
+    public String inferProtocol(int sourcePort, int destinationPort, String str) {
+        // 通过端口号来推断应用协议
+        if (sourcePort == 443 || destinationPort == 443 || str.contains("HTTPS")) {
+            return "HTTPS";
+        } else if (sourcePort == 80 || destinationPort == 80 || str.contains("HTTP")) {
+            return "HTTP";
+        } else if (sourcePort == 53 || destinationPort == 53) {
+            return "DNS";
+        } else {
+            return "Unknown";
+        }
     }
 }
--- src/NPCWindow.java
+++ src/NPCWindow.java
@@ -7,14 +7,15 @@ import java.awt.*;
  * 负责可视化界面的类
  */
 public class NPCWindow extends JFrame {
-    private static DefaultTableModel model;
-    private static JTextField filterField;
-    private JTextArea showArea;
-    private JButton startBtn;
-    private JButton checkDeviceBtn;
-    private JButton exitBtn;
-    private JButton clearBtn;
-
+    public static JTextField filterField;
+    public static DefaultTableModel model;
+    public static JTextArea byteArea;
+    public static JTextArea txtArea;
+    public static JTextArea statusArea;
+    public static JTable table;
+    public JComboBox<String> chooseDevice;
+    public JButton startBtn;
+    public JButton clearBtn;
     /**
      * 窗口的构造方法
      */
@@ -23,14 +24,214 @@ public class NPCWindow extends JFrame {
         createNewWindow();
         setVisible(true);
     }
+
+    /**
+     * 获取filter实例
+     * @return filter实例
+     */
+    public static JTextField getFilterField() {
+        return filterField;
+    }
     /**
      * 新建窗口
      */
     public void createNewWindow() {
         /* 窗口基础设置 */
-        setSize(1300, 800);
+        setSize(1250, 560);
         setTitle("Network Packet Capture");
+        JPanel mainPanel = new JPanel();
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        /* 设置主面板 */
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        add(mainPanel);
+        PanelStyle panelStyle = new PanelStyle();
+
+        /* 窗口顶部 */
+        panelStyle.setTopPanel(this);
+        mainPanel.add(panelStyle.getTopPanel());
+
+        /* 窗口主体 */
+        panelStyle.setTablePanel(this.getWidth());
+        mainPanel.add(panelStyle.getTablePanel());
+
+        /* 添加间距 */
+        mainPanel.add(Box.createVerticalStrut(10));
+
+        /* 窗口底部 */
+        panelStyle.setBottomPanel(this.getWidth());
+        mainPanel.add(panelStyle.getBottomPanel());
+
+        /* 状态栏 */
+        panelStyle.setStatusPanel(this.getWidth());
+        mainPanel.add(panelStyle.getStatusPanel());
+
+        /* 添加间距 */
+        mainPanel.add(Box.createVerticalStrut(10));
+
+        pack();
+    }
+}
+
+/**
+ * 每个板块的样式
+ */
+class PanelStyle {
+    // 最上面的导航栏
+    private JPanel topPanel;
+    // 主体
+    private JPanel tablePanel;
+    // 底部的数据部分
+    private JPanel bottomPanel;
+    // 当前状态
+    private JPanel statusPanel;
+
+    /**
+     * 最上层的panel
+     * @param window 窗口实例
+     */
+    public void setTopPanel(NPCWindow window) {
+        topPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        /* 顶部选择接口的组件 */
+        window.chooseDevice = new JComboBox<String>(new String[]{"choose a device","all"});
+        window.chooseDevice.setPrototypeDisplayValue("11111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+        /* start按钮 */
+        window.startBtn = new JButton("start");
+        /* clear按钮 */
+        window.clearBtn = new JButton("clear");
+        /* filter输入框 */
+        JLabel filterLabel = new JLabel("filter: ");
+        NPCWindow.filterField = new JTextField();
+        NPCWindow.filterField.setPreferredSize(new Dimension(window.getWidth() - window.chooseDevice.getPreferredSize().width - window.startBtn.getPreferredSize().width - window.clearBtn.getPreferredSize().width - filterLabel.getPreferredSize().width, 28));
+        topPanel.add(window.chooseDevice);
+        topPanel.add(window.startBtn);
+        topPanel.add(window.clearBtn);
+        topPanel.add(filterLabel);
+        topPanel.add(NPCWindow.filterField);
+    }
+
+    /**
+     * 主面板的布局，包含抓包的表格条目
+     * @param windowWidth 窗口宽度
+     */
+    public void setTablePanel(int windowWidth) {
+        NPCWindow.model = new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // 不允许编辑任何单元格
+            }
+        };
+        tablePanel = new JPanel();
+        tablePanel.setMaximumSize(new Dimension(windowWidth, 270));
+        tablePanel.setLayout(new BorderLayout()); // 使用边界布局管理器
+
+        /* time stamp    |    source ip   |   dest ip   | pro | len |      info     | */
+        String[] columnNames = {"time stamp", "source ip", "dest ip", "protocol", "length", "info", "bytes", "str"}; // 定义表格列名数组
+        NPCWindow.table = new JTable(NPCWindow.model);
+        DefaultTableModel model = (DefaultTableModel) NPCWindow.table.getModel();
+        model.setColumnIdentifiers(columnNames);
+        JScrollPane scrollPane = new JScrollPane(NPCWindow.table);
+        NPCWindow.table.setEnabled(true);
+        // 手动调整列宽度
+        NPCWindow.table.getColumnModel().getColumn(0).setPreferredWidth(windowWidth * 2 / 24);
+        NPCWindow.table.getColumnModel().getColumn(1).setPreferredWidth(windowWidth * 5 / 24);
+        NPCWindow.table.getColumnModel().getColumn(2).setPreferredWidth(windowWidth * 5 / 24);
+        NPCWindow.table.getColumnModel().getColumn(3).setPreferredWidth(windowWidth * 3 / 24);
+        NPCWindow.table.getColumnModel().getColumn(4).setPreferredWidth(windowWidth * 3 / 24);
+        NPCWindow.table.getColumnModel().getColumn(5).setPreferredWidth(windowWidth * 6 / 24);
+        // 隐藏最后两列
+        NPCWindow.table.getColumnModel().getColumn(6).setMaxWidth(0);
+        NPCWindow.table.getColumnModel().getColumn(6).setMinWidth(0);
+        NPCWindow.table.getColumnModel().getColumn(6).setWidth(0);
+        NPCWindow.table.getColumnModel().getColumn(6).setResizable(false);
+        NPCWindow.table.getColumnModel().getColumn(6).setPreferredWidth(0);
+        NPCWindow.table.getColumnModel().getColumn(6).setHeaderValue(""); // 可选，清除列的标题
+
+        NPCWindow.table.getColumnModel().getColumn(7).setMaxWidth(0);
+        NPCWindow.table.getColumnModel().getColumn(7).setMinWidth(0);
+        NPCWindow.table.getColumnModel().getColumn(7).setWidth(0);
+        NPCWindow.table.getColumnModel().getColumn(7).setResizable(false);
+        NPCWindow.table.getColumnModel().getColumn(7).setPreferredWidth(0);
+        NPCWindow.table.getColumnModel().getColumn(7).setHeaderValue(""); // 可选，清除列的标题
+
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED); // 根据需要显示垂直滚动条
+        scrollPane.setPreferredSize(new Dimension(windowWidth, 270));
+          // 在添加数据后，滚动条自动滚动到最后一行
+//        NPCWindow.table.getModel().addTableModelListener(new TableModelListener() {
+//            @Override
+//            public void tableChanged(TableModelEvent e) {
+//                SwingUtilities.invokeLater(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        // 获取滚动条对象
+//                        JScrollBar verticalScrollBar = scrollPane.getVerticalScrollBar();
+//                        // 设置滚动条位置
+//                        verticalScrollBar.setValue(verticalScrollBar.getMaximum());
+//                    }
+//                });
+//            }
+//        });
+        tablePanel.add(scrollPane, BorderLayout.CENTER); // 将滚动面板添加到中间位置
+    }
+
+    /**
+     * 下面的区域：16进制码区域和字符区域
+     * @param windowSize 窗口大小
+     * */
+    public void setBottomPanel(int windowSize) {
+        bottomPanel = new JPanel();
+        bottomPanel.setPreferredSize(new Dimension(windowSize, 200));
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
+        NPCWindow.byteArea = new JTextArea();
+        NPCWindow.txtArea = new JTextArea();
+
+        NPCWindow.byteArea.setLineWrap(true);
+        NPCWindow.txtArea.setLineWrap(true);
+
+        NPCWindow.byteArea.setEditable(false);
+        NPCWindow.txtArea.setEditable(false);
+
+        // 设置 JTextArea 的首选大小
+        NPCWindow.byteArea.setMaximumSize(new Dimension(windowSize / 2 - 15, 200));
+        NPCWindow.txtArea.setMaximumSize(new Dimension(windowSize / 2 - 15, 200));
+
+        JScrollPane byteScrollPane = new JScrollPane(NPCWindow.byteArea);
+        JScrollPane txtScrollPane = new JScrollPane(NPCWindow.txtArea);
+        byteScrollPane.setPreferredSize(new Dimension(windowSize / 2 - 15, 200));
+        txtScrollPane.setPreferredSize(new Dimension(windowSize / 2 - 15, 200));
+
+        byteScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        txtScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        // 将 JScrollPane 添加到 bottomPanel
+        bottomPanel.add(Box.createHorizontalStrut(20));
+        bottomPanel.add(byteScrollPane);
+        bottomPanel.add(Box.createHorizontalStrut(10));
+        bottomPanel.add(txtScrollPane);
+        bottomPanel.add(Box.createHorizontalStrut(20));
+    }
+    public void setStatusPanel(int windowSize) {
+        statusPanel = new JPanel();
+        statusPanel.setLayout(new FlowLayout());
+        NPCWindow.statusArea = new JTextArea();
+        NPCWindow.statusArea.setLineWrap(true);
+        NPCWindow.statusArea.setPreferredSize(new Dimension(windowSize, 70));
+
+        statusPanel.add(Box.createHorizontalStrut(10));
+        statusPanel.add(NPCWindow.statusArea);
+        statusPanel.add(Box.createHorizontalStrut(10));
+    }
+    public JPanel getTopPanel() {
+        return topPanel;
+    }
+    public JPanel getTablePanel() {
+        return tablePanel;
+    }
+    public JPanel getBottomPanel() {
+        return bottomPanel;
+    }
+    public JPanel getStatusPanel() {
+        return statusPanel;
     }
 }
